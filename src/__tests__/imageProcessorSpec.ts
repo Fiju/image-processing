4d0f9d4f2e4d56d6abd9b547bd43528ab3785046
Fiju <faixanshirax@gmail.com>
2021-07-11 04:28:41 +0500

    Extra tests

--- src/__tests__/imageProcessorSpec.ts
+++ src/__tests__/imageProcessorSpec.ts
@@ -1,10 +1,21 @@
 "use strict";
 
+import path from "path";
 import pathNormalizer, { IResult } from "../helpers/path-normalizer";
 
 describe("Image exist check", () => {
-  it("should", async () => {
+  it("It should return result telling image is available", async () => {
     const fileData: IResult = pathNormalizer("luffy", undefined, undefined);
-    expect(fileData).toEqual({ error: "Image not found" });
+    expect(fileData).toEqual({
+      path: path.resolve(__dirname, "../../assets/luffy.jpg"),
+      isAvailable: true,
+    });
+  });
+
+  it("It should return result telling image is not available", async () => {
+    const fileData: IResult = pathNormalizer("missing", undefined, undefined);
+    expect(fileData).toEqual({
+      error: "Image not found",
+    });
   });
 });
