a19a4313990fff0780afd89ebaa339ea99e9ede7
Fiju <faixanshirax@gmail.com>
2021-07-11 08:47:34 +0500

    Jimp => Sharp

--- src/middlewares/image-processing.ts
+++ src/middlewares/image-processing.ts
@@ -1,25 +1,28 @@
 import { Request, Response } from "express";
-import { createReadStream } from "fs";
-import Jimp from "jimp";
+import { promises as fsPromise } from "fs";
+import * as fs from "fs";
+import sharp from "sharp";
 
-const processesor = async (req: Request, res: Response, next: Function) => {
+const processesor = async (req: Request, res: Response) => {
   const { query } = req;
-  await Jimp.read(
-    require("path").resolve(__dirname, `../../assets/${query.name}.jpg`)
-  )
-    .then((img) => {
-      const w: number = Number(query.width);
-      const h: number = Number(query.height);
-      img.resize(w, h).write(res.locals.path);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  const r = createReadStream(res.locals.path);
-  res.type("jpg").status(200);
-  r.pipe(res);
-  res.send(r);
-  next();
+  const w: number = Number(query.width);
+  const h: number = Number(query.height);
+  await fsPromise.mkdir(
+    require("path").resolve(__dirname, "../../assets/thumbnails/"),
+    { recursive: true }
+  );
+  fs.writeFile(
+    res.locals.path,
+    await sharp(
+      require("path").resolve(__dirname, `../../assets/${query.name}.jpg`)
+    )
+      .resize(w, h)
+      .toBuffer(),
+    (err: NodeJS.ErrnoException | null): void => {
+      if (!err) res.sendFile(res.locals.path);
+      else res.status(500).send("There was an error processing the image");
+    }
+  );
 };
 
 export default processesor;
